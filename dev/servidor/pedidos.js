dd784478b79db29c4ab10349f178db5bceaefdca
root <nunnes00@gmail.com>
2016-07-05 22:03:03 +0100

    comunicação com couchdb \n falta enviar resultado do pedido para o lado do cliente

---
+++ dev/servidor/pedidos.js
@@ -0,0 +1,26 @@
+module.exports = {
+        iniciar: function () {
+        this.objPersonalizados();
+    },
+    
+      objPersonalizados: function () {
+        /**
+         * Substitui todas as ocorrências 
+         * @param {string} procura Texto a procurar
+         * @param {string} substitui Texto a substituir
+         * @returns {string} Texto final transformado
+         */
+        String.prototype.substituiTudo = function (procura, substitui) {
+            var target = this;
+            return target.replace(new RegExp(procura.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), 'g'), substitui);
+        };
+    },
+        
+        loadCats: function (json) {
+             console.log(json);
+         var xhttp = new XMLHttpRequest();
+        
+         xhttp.open("GET", json, true);
+         xhttp.send();
+       }
+};
